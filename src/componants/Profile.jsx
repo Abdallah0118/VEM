7c5307395b9628e29df3a6f8c481a1c5cae45a9d
ahmed mostafa mohamed <103125286+hijazy1@users.noreply.github.com>
2023-06-24 20:35:36 +0300

    Update Profile.jsx

--- src/componants/Profile.jsx
+++ src/componants/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./CSS/Profile.css";
-import { Card,Button, Col ,Row,Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 const Profile  = () => {
 // ================================================================================================
 const [userInfo, setUserInfo] = useState(null);
@@ -27,68 +28,55 @@ useEffect(() => {
   <div className="col-6 col-md-4">
   <div className="container mt-2 w-100">
     <div className="row d-flex justify-content-center">
-        <div className="col h-50 mt-3 mb-3">
-            <div className="card p-3  ">
+        <div className="col h-50 mb-3">
+            <div className="card p-1  ">
             {userInfo ? (
               <>
                 <div className="text-center">
-                    <img src={  `http://localhost:8000/${userInfo.User.profile_picture}`||require("../assets/user.jpg")} width="180" alt="UserPhoto" className="rounded-circle"/>
+                    <img src={userInfo.User.profile_picture ? `http://localhost:8000/${userInfo.User.profile_picture}` : require("../assets/user.jpg")} width="150" alt="UserPhoto" className="rounded-circle"/>
                 </div>
-                       <div className="text-center p-3">
-                    <span className="bg-secondary p-1 px-4 rounded text-white" style={{display: userInfo.User.is_seller ? "inline" : "none"}}>Pro</span>
+                    <div className="text-center p-1">
+                    <h5 className="mt-2 mt-1"> {userInfo.User.first_name} {userInfo.User.last_name}</h5>
+                    <span className=".text-primary p-1 px-4 rounded " style={{display: userInfo.User.is_seller ? "inline" : "none"}}>Seller</span>
                     <img src={require("../assets/quality.png")} width="30" alt="quality" style={{display: userInfo.User.is_active? "inline" : "none"}}/>
-                    
-                    <h5 className="mt-2 mb-0"> {userInfo.User.first_name} {userInfo.User.last_name}</h5>
-                    {/* <span>UI/UX Designer</span> */}
-                    
-                    <div className="p-2 ">
+                    {/* <span>UI/UX Designer</span> */}                    
+                    {/* <div className="p-2 "> */}
                         {/* <p className="fonts">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> */}
                     </div  >
                     <form  method="GET">
-                      {/* <div className="d-flex justify-content-between ">
-                      <div className="txt_field  " >
-                        <input type="text" value={userInfo.User.first_name || "no name" }   autofocus/>
-                        <span></span>
-                        <label>first Name</label>
-                      </div>
-                      <div className="txt_field ">
-                        <input type="text"  value={userInfo.User.last_name || "no name"}   autofocus/>
-                        <span></span>
-                        <label>Last Name</label>
-                      </div>
-                      </div> */}
                       <div className="txt_field1">
-                        <input type="email"  value={userInfo.User.email || "No email"}  readOnly autofocus />
+                        <input type="email"  value={userInfo.User.email || "No email"}  readOnly />
                         <span></span>
                         <label>Email</label>
                       </div>
                       <div className="txt_field1">
-                        <input type="tel"  value={userInfo.User.phone|| "no name"}  readOnly autofocus/>
+                        <input type="tel"  value={userInfo.User.phone|| "no name"}  readOnly />
                         <span></span>
                         <label>Phone</label>
                       </div>
                       <div className="txt_field1">
-                        <input type="text"   value={userInfo.User.birth_date|| "no name"}  readOnly autofocus/>
+                        <input type="text"   value={userInfo.User.birth_date|| "no name"}  readOnly />
                         <span></span>
                         <label>Birth Date</label>
                       </div>
                       <div className="txt_field1">
-                        <input type="text"  value={userInfo.User.location|| "no name"}  readOnly autofocus/>
+                        <input type="text"  value={userInfo.User.location|| "no name"}  readOnly />
                         <span></span>
                         <label>Location</label>
                       </div>
                       <div className="txt_field1">
-                        <input type="password" value={userInfo.User.password|| "no name"} readOnly autofocus/>
+                        <input type="password" value={userInfo.User.password|| "no name"} readOnly />
                         <span></span>
                         <label>Password</label>
                       </div>
                     </form>
-                    <img  src={ require("../assets/edit-button.png")} width={"70px"} alt="edit profile" className="btn btn-light  edit1" />
-                    
-                </div>
+                    <Link to="/profile/edit">
+                    <img  src={ require("../assets/edit-button.png")} width={"70px"} alt="edit profile" className="btn btn-light  edit1" />  
+                    </Link>
+                {/* </div> */}
                 </>   
                 ) : (   
-                  <div className="text-center">
+                  <div className="text-center p-5">
                   <div className="spinner-border" role="status">
                     <span className="visually-hidden">Loading...</span>
                   </div>
@@ -110,7 +98,7 @@ useEffect(() => {
     </blockquote>
     </div>
                 ) : (   
-                  <div className="text-center">
+                  <div className="text-center p-5">
                   <div className="spinner-border" role="status">
                     <span className="visually-hidden">Loading...</span>
                   </div>
